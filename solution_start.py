c5dcb727e67e71d972115a5273bb5bd633a3a6c2
SidharthMav <mavsidharth@gmail.com>
2022-07-18 00:37:02 +0530

    Minor changes
    - fixed function name
    - tested again

--- solution_start.py
+++ solution_start.py
@@ -20,27 +20,27 @@ def get_params() -> dict:
     args, unknown = parser.parse_known_args()
     return args
 
-def getListOfFiles(dirName):
+def get_list_of_files(dir_name):
     # create a list of file and sub directories names in the given directory 
-    listOfFile = os.listdir(dirName)
-    allFiles = list()
+    list_of_file = os.listdir(dir_name)
+    all_files = list()
     # Iterate over all the entries
-    for entry in listOfFile:
+    for entry in list_of_file:
         # Create full path
-        fullPath = os.path.join(dirName, entry)
+        full_path = os.path.join(dir_name, entry)
         # If entry is a directory then get the list of files in this directory 
-        if os.path.isdir(fullPath):
-            allFiles = allFiles + getListOfFiles(fullPath)
+        if os.path.isdir(full_path):
+            all_files = all_files + get_list_of_files(full_path)
         else:
-            allFiles.append(fullPath)
+            all_files.append(full_path)
                 
-    return allFiles
+    return all_files
 
 def get_file_paths(path, list_of_files, extension):
     # search 'path' parent directory and child directories
     # append the path of files with specific extension to 'list_of_files'
     
-    for file in getListOfFiles(path):
+    for file in get_list_of_files(path):
         if os.path.splitext(file)[-1]==extension:
             list_of_files.append(file)
     return list_of_files
